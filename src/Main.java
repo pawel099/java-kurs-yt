b8b1b57c1f2d11c0541661350b90863ea3aec91e
pawel123 <pawelek78-30@o2.pl>
2023-08-19 14:16:23 +0200

    pierwszy program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
+/* proste ćwiczenie  instrukcja warunkowa switch case */
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner obiejcts = new Scanner(System.in);
 
+        String dane  = obiejcts.nextLine();
+
+           switch (dane) {
+
+               case "pawel":
+                   System.out.println("Hello pawel");
+                   break;
+
+               case "anna":
+
+                   System.out.println("Hello anna");
+                   break;
+
+               case "piotr":
+
+                   System.out.println("Hello piotr");
+                   break;
+
+               case "michal":
+
+                   System.out.println("Hello michal");
+                   break;
+
+               default:
+                   System.out.println("witaj nieznajomy");
+
+
+
+
+           }
     }
 }
