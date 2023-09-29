c3919d06641d16cc136d65abe6860e01ffd0b321
pawel123 <pawelek78-30@o2.pl>
2023-09-29 08:30:15 +0200

    typy proste

---
+++ src/lekcja0.java
@@ -0,0 +1,25 @@
+public class lekcja0 {
+    // typy proste
+
+    public static void main(String[] args) {
+
+        int liczba=3;
+        double liczba1=123D;
+        float liczba2=12F;
+        long liczba3=128L;
+        short liczba4=2;
+        boolean zmienna =true;
+        byte bajt =1;
+        char znak ='@';
+
+
+
+        //liczby
+
+        System.out.println(liczba + liczba1 + liczba2 + liczba3 + liczba4);
+        System.out.println(zmienna);
+        System.out.println(bajt);
+        System.out.println(znak);
+
+    }
+}
