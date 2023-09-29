43b6190c61ff3ea06343456efacf988f60fead7d
pawel123 <pawelek78-30@o2.pl>
2023-09-29 08:58:55 +0200

    aplikacja

--- src/lekcja5.java
+++ src/lekcja5.java
@@ -1,58 +1,51 @@
-
-import java.awt.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import javax.swing.*;
-/* zadanie zgadnij imię po wpisaniu tekstu w konsoli + rozwiązanie */
+public class lekcja5 {
 
-public class lekcja5 extends JFrame {
-    protected JFrame frame;
-    protected JOptionPane optionPane;
-
-    private void frames() {
-
-      frame = new JFrame() ;
-      frame.setTitle("podaj imię");
-      frame.setSize(500,300);
-    }
-    private void showMessage() {
-
-        Scanner outs = new Scanner(System.in);
-        String wynik = outs.nextLine();
-
-
-        switch (wynik) {
-
-            case "pawel":
-
-                  JOptionPane.showMessageDialog(frame,"hello pawel");
-
-                   break;
-
-                   case "andrzej":
-                       JOptionPane.showMessageDialog(frame, "hello andriu");
-                   break;
-
-                   case "anna":
-                       JOptionPane.showMessageDialog(frame, "hello anna");
-                   break;
-
-                   case "michal":
-                       JOptionPane.showMessageDialog(frame, "hello michal");
-
-                  case "asia":
-                  JOptionPane.showMessageDialog(frame, "hello Joanna");
-
-                   break;
-
-                   default: System.out.println("niestety nie zgadłeś , spróbuj ponownie");
-        }
+    private int kwadrat(int wynik) {
+            int suma = ( 4 * wynik);
+            return suma;
     }
 
-    public static void main (String[] args) {
 
-        lekcja5 dane = new lekcja5();
-        dane.frames();
-        dane.showMessage();
-
+    private double prostokat(double x ,double y) {
+        double suma =2*x + 2* y;
+        return suma;
     }
- }
+
+    public static void main(String[] args) {
+
+        lekcja5 obliczPole = new lekcja5();
+
+        try {
+
+             Scanner inputs = new Scanner(System.in);
+
+             /* System.out.print("oblicz d\u0142ugo\u015B\u0107 boku kwadratu aby wyliczy\u0107 jego objeto\u015B\u0107: ");
+             int dane = inputs.nextInt();
+             System.out.print("obwód pola kwadratu to " + obliczPole.kwadrat(dane));
+             */
+
+            /* System.out.print("podaj długość boku A prostokąta: ");
+             double sideLengthA = inputs.nextDouble();
+
+
+             System.out.print("podaj długość boku B prostokąta: ");
+             double sideLengthB = inputs.nextDouble();
+
+             System.out.print("długość boku prostokęta A I B to " + obliczPole.prostokat(sideLengthA,sideLengthB));
+            */
+
+            System.out.print("porównaj wartość A ");
+            int sideLengthC = inputs.nextInt();
+            System.out.print("porównaj wartość B ");
+            int sideLengthL = inputs.nextInt();
+            System.out.print("A I B jest " +  (sideLengthC==sideLengthL));
+
+          }
+
+         catch (InputMismatchException e) {
+               System.out.print("podane dane s\u0105 b\u0142edne");
+       }
+    }
+}
