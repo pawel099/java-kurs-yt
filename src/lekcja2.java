54c2d43cd9c2ac7964ce635b90916c43593c30eb
pawel123 <pawelek78-30@o2.pl>
2023-09-29 08:03:04 +0200

    operatory artmetyczne

--- src/lekcja2.java
+++ src/lekcja2.java
@@ -1,8 +1,7 @@
-
 import java.util.Scanner;
 
 public class lekcja2 {
-    /* zadanie wyswietl liczby podzielne przez 2 i rozwiązanie */
+    /* operatory artmetyczne  */
 
     public static void main(String[] args) {
 
@@ -15,8 +14,40 @@ public class lekcja2 {
 
             index++;
         }
-    }
-}
+
+            int l1 = 1;
+            int l2 = 1 ;
+
+
+            int suma = l1 + l2;
+
+            try {
+
+                System.out.println(suma/0);
+
+            }  catch(ArithmeticException e) {
+
+                System.out.println("mała cholero nie dziel przez zero");
+            }
+
+
+            int i=0;
+
+            int inkrementacja=100;
+
+             while (i<10) {
+
+                 if (i!=0) {
+
+                    int x = inkrementacja-index;
+                    System.out.println(x);
+                }
+
+                i++;
+            }
+         }
+     }
+
 
 
 
