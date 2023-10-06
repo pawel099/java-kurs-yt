8c1d8a3a340e7f96d34fbc74f524a8bd6e8f87bd
pawel123 <pawelek78-30@o2.pl>
2023-10-06 12:51:02 +0200

    instrukcje warunkowe if switch-case

---
+++ src/lekcja7.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class lekcja7 {
+
+    private Scanner scanner ;
+
+    protected int instrukcjeWarunkowe(int wyniki) {
+
+
+
+            if (wyniki == 1) {
+
+                scanner = new Scanner(System.in);
+
+                System.out.print("oblicz d\u0142ugo\u015B\u0107 boku kwadratu aby wyliczy\u0107 jego objeto\u015B\u0107: ");
+                int x = scanner.nextInt();
+                int suma = (4 * x);
+
+                return suma;
+
+
+
+            } else if (wyniki == 2) {
+
+                scanner = new Scanner(System.in);
+
+                System.out.print("podaj długość boku A prostokąta: ");
+
+                int x = scanner.nextInt();
+                System.out.print("podaj długość boku B prostokąta: ");
+
+                int y = scanner.nextInt();
+
+                int suma = 2 * x + 2 * y;
+                return suma;
+            }
+
+
+            return 0;
+
+
+    }
+    private int switchCase(int f) {
+
+        switch(f) {
+
+
+
+                case 1:
+                    scanner = new Scanner(System.in);
+
+                    System.out.print("oblicz d\u0142ugo\u015B\u0107 boku kwadratu aby wyliczy\u0107 jego objeto\u015B\u0107: ");
+                    int c = scanner.nextInt();
+                    int suma = (4 * c);
+
+                    return suma;
+
+
+                case 2:
+
+                    scanner = new Scanner(System.in);
+                    System.out.print("podaj długość boku A prostokąta: ");
+                    int x = scanner.nextInt();
+
+                    System.out.print("podaj długość boku B prostokąta: ");
+
+                    int y = scanner.nextInt();
+                    int d = 2 * x + 2 * y;
+                    return d;
+
+            }
+
+
+        return 0;
+    }
+
+     public static void main(String[] args)  {
+
+        Scanner initialize  = new Scanner(System.in);
+        int y = initialize.nextInt();
+        lekcja7 wynik = new lekcja7();
+
+
+    System.out.println(wynik.switchCase(y));
+
+}
+
+}
