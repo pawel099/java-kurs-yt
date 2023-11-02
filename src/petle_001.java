26edc808f897a3cf6dcb582363b31414cea98cc8
pawel123 <pawelek78-30@o2.pl>
2023-11-02 13:27:39 +0100

    petle zagniezdzone

--- src/petle_001.java
+++ src/petle_001.java
@@ -1,3 +1,48 @@
+import java.util.Scanner;
+
 public class petle_001 {
+
+    public void listaUzytkownikow() {
+
+        String[][] lista = {
+
+                {"lista pracowników"},
+
+                {"anna"," - pracownik biurowy "},
+                {"michal"," - Starszy Programista" },
+                {"piotr", " - kierownik działu"},
+                {"janek" , " - HR"},
+                {"regina"," - młodszy programista"},
+          };
+
+        for (int loop_lista=0;loop_lista<lista.length;loop_lista++) {
+            for (int loop_w = 0; loop_w < lista[loop_lista].length; loop_w++) {
+
+                System.out.print(lista[loop_lista][loop_w] + " ");
+                System.out.println();
+
+            }
+        }
+    }
+
+    public void choinka() {
+
+        Scanner scanner = new Scanner(System.in);
+        int dane = scanner.nextInt();
+
+        for (int index = 0; index < dane; index++) {
+            for (int i = 0; i <= index; i++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+     }
+
+
+    public static void main(String[] args) {
+    petle_001 dane = new petle_001();
+        dane.listaUzytkownikow();
+       // dane.choinka();
+    }
 
 }
