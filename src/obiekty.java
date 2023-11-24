bfce841cf2ae2dd64cf117ce6d3ea673fbe34828
pawel123 <pawelek78-30@o2.pl>
2023-11-24 14:15:15 +0100

    metody, rekurencja + prosta aplikacja

--- src/obiekty.java
+++ src/obiekty.java
@@ -1,2 +1,11 @@
-public class obiekty {
+import java.util.*;
+public class obiekty extends aplikacja {
+    public static void main(String[] args) {
+
+        obiekty dane = new obiekty();
+        int c = dane.wyniki(new Scanner(System.in));
+        System.out.print("odgadłeś za " + " " +  c + " razem ");
+
+
+    }
 }
