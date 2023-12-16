efb12de0e32ae54077d1f826e5608a5458ce3269
pawel123 <pawelek78-30@o2.pl>
2023-12-16 14:52:42 +0100

    klasy abstrakcyjne

--- src/klasyAbstrakcyjne/klasa1.java
+++ src/klasyAbstrakcyjne/klasa1.java
@@ -1,25 +1,9 @@
 package klasyAbstrakcyjne;
 
 abstract class klasa1 {
-    protected String DB_CONNECTION ;
-    protected String DB_HOST ;
-    protected String DB_DATABASE ;
-    protected String DB_USERNAME;
 
-    protected String BROADCAST_DRIVER="log1";
-    protected String CACHE_DRIVER;
-    protected String FILESYSTEM_DISK;
-
-    protected void connection() {
-
-        System.out.println(this.DB_CONNECTION="mysql ");
-        System.out.println(this.DB_HOST="127.0.0.1 ");
-        System.out.println(this.DB_DATABASE=" baza ");
-        System.out.println(this.DB_USERNAME=" root ");
-        System.out.println(this.BROADCAST_DRIVER=" log");
-    }
-
-
-    abstract void BROADCAST_DRIVER();
-
+    abstract void dane1();
+    abstract void dane2();
+    abstract void dane3();
+    abstract void dane4();
 }
--- src/klasyAbstrakcyjne/main.java
+++ src/klasyAbstrakcyjne/main.java
@@ -2,16 +2,28 @@ package klasyAbstrakcyjne;
 
 public class main extends klasa1 {
 
-   public void BROADCAST_DRIVER() {
-      System.out.println(this.BROADCAST_DRIVER="log2");
+   public void dane1() {
 
+       System.out.println("hello");
+   }
+   public void dane2() {
+       System.out.println("hello");
+   }
+
+
+    public void dane3() {
+       System.out.println("hello");
+   }
+
+
+    public void dane4() {
+       System.out.println("hello");
    }
 
    public static void main(String[] args) {
 
     main program = new main();
-    program.connection();
-    program.BROADCAST_DRIVER();
+    program.dane1();
 
     }
 }
