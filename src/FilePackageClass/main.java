77d289c23b7af02b82f16dc7f9df7ed399fcaab0
pawel123 <pawelek78-30@o2.pl>
2024-01-11 13:21:44 +0100

    zapis do pliku

--- src/FilePackageClass/main.java
+++ src/FilePackageClass/main.java
@@ -1,6 +1,30 @@
 package FilePackageClass;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class main {
 
- msmsmmsm ddnnd
+ private void files(FileWriter file ,String wynik) throws IOException {
+  file.write(wynik + "\n");
+  file.flush();
+ }
+
+ public void dopisz(FileWriter file ,String wynik) throws IOException {
+  file.append(wynik + "\n");
+  file.flush();
+
+ }
+ public static void main(String[] args) throws IOException {
+
+  Scanner scanner = new Scanner(System.in);
+  String zapisz = scanner.nextLine();
+  main dane = new main();
+
+  final String FILE_WRITE = "src/file/plik.txt";
+  FileWriter files = new FileWriter(FILE_WRITE,true);
+  dane.dopisz(files,zapisz);
+
+}
+
 }
