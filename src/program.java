134599b09f73128a282fa3d273179dbbee23dc4f
pawel123 <pawelek78-30@o2.pl>
2024-02-24 14:17:26 +0100

    mnozenie parzystych elementów tablicy

---
+++ src/program.java
@@ -0,0 +1,23 @@
+public class program {
+    public int int_num_value=0;
+
+    protected int wynik() {
+
+        int[] tablica_elementow = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int index = 0; index < tablica_elementow.length; index++) {
+
+            if (index % 2 == 0) {
+                int x = index * tablica_elementow[index];
+                this.int_num_value = x;
+            }
+        }
+        return this.int_num_value ;
+    }
+    public static void main(String[] args) {
+
+      program e = new program();
+      System.out.println(e.wynik());
+
+    }
+ }
